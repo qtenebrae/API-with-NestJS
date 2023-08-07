97b933b4257ccc8bd8c5f4b583bdd737354937da
qtenebrae <serega.ivanovv2012@gmail.com>
2023-08-07 18:26:50 +0300

    Dividing the project into modules, creating models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TopPageModule } from './top-page/top-page.module';
+import { ReviewModule } from './review/review.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [AuthModule, TopPageModule, ReviewModule, ProductModule],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
