c9fdb7278a6b8e3a48906b6edf01093733aba3fa
qtenebrae <serega.ivanovv2012@gmail.com>
2023-08-15 15:32:38 +0300

    Implement review service

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,20 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpException,
+	HttpStatus,
+	Param,
+	Post,
+} from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewService } from './review.service';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { DocumentType } from '@typegoose/typegoose/lib/types';
 import { ReviewModel } from './review.model/review.model';
 
 @Controller('review')
 export class ReviewController {
+	constructor(private readonly reviewService: ReviewService) {}
+
 	@Post('create')
-	async create(@Body() dto: Omit<ReviewModel, '_id'>): Promise<void> {
-		return null;
+	async create(@Body() dto: CreateReviewDto): Promise<DocumentType<ReviewModel>> {
+		return this.reviewService.create(dto);
 	}
 
 	@Delete(':id')
 	async delete(@Param('id') id: string): Promise<void> {
-		return null;
+		const deletedDoc = await this.reviewService.delete(id);
+		if (!deletedDoc) {
+			throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+		}
 	}
 
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId') productId: string): Promise<void> {
-		return null;
+	async getByProduct(@Param('productId') productId: string): Promise<DocumentType<ReviewModel>[]> {
+		return this.reviewService.findByProductId(productId);
 	}
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
 import { ReviewModel } from './review.model/review.model';
 import { TypegooseModule } from '@m8a/nestjs-typegoose';
+import { ReviewService } from './review.service';
 
 @Module({
 	controllers: [ReviewController],
@@ -15,5 +16,6 @@ import { TypegooseModule } from '@m8a/nestjs-typegoose';
 			},
 		]),
 	],
+	providers: [ReviewService],
 })
 export class ReviewModule {}
